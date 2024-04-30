098fb9d1469644873659515f97984e429a883152
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-04-30 15:08:30 +0200

    Task 3 - fisnd and fix bug

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -6,6 +6,21 @@
 //    std::cout << "I love C++\n";
 //}
 
-//task2 - the smalest program that compile
-int main() {
+////task2 - the smalest program that compile
+//int main() {
+//}
+
+
+//task 3 - find and fix a bug
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//this program doesn`t work because it missed first " and ; in the end
+	//cout << Is there a bug here ? "
+
+	//fixed string
+	cout << "Is there a bug here ?";
+	return 0;
 }
