b3a36757c25f11d377f018f94600ee41cdfe4cd6
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-05-03 12:19:14 +0200

    HW 3 - variables and constants

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -11,16 +11,28 @@
 //}
 
 
-//task 3 - find and fix a bug
-#include <iostream>
-using namespace std;
+////task 3 - find and fix a bug
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//this program doesn`t work because it missed first " and ; in the end
+//	//cout << Is there a bug here ? "
+//
+//	//fixed string
+//	cout << "Is there a bug here ?";
+//	return 0;
+//}
 
-int main()
-{
-	//this program doesn`t work because it missed first " and ; in the end
-	//cout << Is there a bug here ? "
 
-	//fixed string
-	cout << "Is there a bug here ?";
-	return 0;
+//Homework 3
+int main() {
+	unsigned short myAge = 21;
+	float square = 15.7;
+	unsigned long numOfStars = 200000000000;
+	int mmRain = 632; //because it counts in mm, dont have fraction
+
+	const double PI = 3.14159;
+	float pi = PI;
 }
