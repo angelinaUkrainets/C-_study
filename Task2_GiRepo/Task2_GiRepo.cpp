260d549094d33d796df9b1d1a8206797b8267a9e
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-05-10 18:12:20 +0200

    HW 4 - operators

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,38 +1,84 @@
-//#include <iostream>
-//
-////task1
-//int main()
-//{
-//    std::cout << "I love C++\n";
-//}
+#include <iostream>
 
-////task2 - the smalest program that compile
-//int main() {
-//}
+using namespace std;
 
-
-////task 3 - find and fix a bug
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	//this program doesn`t work because it missed first " and ; in the end
-//	//cout << Is there a bug here ? "
-//
-//	//fixed string
-//	cout << "Is there a bug here ?";
-//	return 0;
-//}
-
-
-//Homework 3
 int main() {
-	unsigned short myAge = 21;
-	float square = 15.7;
-	unsigned long numOfStars = 200000000000;
-	int mmRain = 632; //because it counts in mm, dont have fraction
+	//task1
+	cout << "HW 4 - operations\n\nTask 1\n";
 
-	const double PI = 3.14159;
-	float pi = PI;
+	int num1, num2;
+	cout << "Input first number : ";
+	cin >> num1;
+	cout << "Input second number : ";
+	cin >> num2;
+
+	if (num1 > num2)
+	{
+		num1 = num2;
+		cout << "First number is bigger\n";
+	}
+	else {
+		num2 = num1;
+		cout << "Second number is bigger\n";
+	}
+
+	//task2-3
+	//description : програма просить ввести 3 числа
+	//дал≥ в оператор≥ ≥ф прогама присвоюЇ р≥зницю а-б зм≥нн≥й с.
+	//€кби в оператор≥ було 2 дор≥внюЇ тод≥ б в≥н просто перев≥р€в чи р≥зниц€ а ≥ б р≥вна с.
+
+	cout << "\nTask2-3\n";
+
+	int a, b, c;
+
+	cout << "Please enter three numbers) \n";
+
+	cout << "a: ";
+
+	cin >> a;
+
+	cout << "\nb: ";
+
+	cin >> b;
+
+	cout << "\nc: ";
+
+	cin >> c;
+
+	if (c = (a - b))
+
+	{
+
+		cout << "\na: ";
+
+		cout << a;
+
+		cout << " minus b: ";
+
+		cout << b;
+
+		cout << " equals c: ";
+
+		cout << c << endl;
+
+	}
+
+	else
+
+		cout << "a - b does not equal c: " << endl;
+
+	//task4-5
+	//в оператор≥ програма буде присвоювати зм≥нн≥й с р≥зницю а ≥ б. 
+	//тобто на виход≥ ми отримаЇмо 0, але в ≥ф програма не зайде, так €к результат 0
+
+	cout << "\nTask4-5\n";
+
+	a = 1; 
+	b = 1;
+
+	if (c = (a - b))
+
+		cout << "The value of c is: " << c;
+
+	return 0;
 }
