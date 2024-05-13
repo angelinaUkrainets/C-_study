48a884f442baafb550535f2096f963a31c34ea4a
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-05-13 18:51:14 +0200

    HW 5 - functions

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,38 +1,47 @@
-//#include <iostream>
-//
-////task1
-//int main()
-//{
-//    std::cout << "I love C++\n";
-//}
+#include <iostream>
 
-////task2 - the smalest program that compile
-//int main() {
-//}
+using namespace std;
 
+//task1 - prototype of func perimeter
+unsigned long Perimetr(unsigned short, unsigned short);
 
-////task 3 - find and fix a bug
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	//this program doesn`t work because it missed first " and ; in the end
-//	//cout << Is there a bug here ? "
-//
-//	//fixed string
-//	cout << "Is there a bug here ?";
-//	return 0;
-//}
-
-
-//Homework 3
-int main() {
-	unsigned short myAge = 21;
-	float square = 15.7;
-	unsigned long numOfStars = 200000000000;
-	int mmRain = 632; //because it counts in mm, dont have fraction
-
-	const double PI = 3.14159;
-	float pi = PI;
+//task5
+double Division(unsigned short x, unsigned short y) {
+	if (y == 0)
+		return -1;
+	return x / y;
 }
+
+int main() {
+	auto perimeter = Perimetr(10, 2);
+	cout << "Perimeter : " << perimeter << endl;
+
+	//task6
+	unsigned short x, y;
+	double res = 0;
+
+	cout << "Please, enter two numbers\n";
+	cin >> x;
+	cin >> y;
+	res = Division(x, y);
+	cout << "Result of division is : " << res << endl;
+}
+
+//task2 - counting of perimeter
+unsigned long Perimetr(unsigned short length, unsigned short width)
+{
+	cout << "Rectangle:\nLength : " << length << endl;
+	cout << "Width : " << width << endl;
+
+	return length*2+width*2;
+}
+
+
+//task3 - bugs
+//змінна х не ініціалізована - містить в собі якусь комірку з памяті
+//при передачі параметрів в функцію прописаний тільки тип даних, але функція не зможе порахувати, тому що не знає що саме їй потрібно
+//а основне функція типу воід не може нічього повертати
+
+//task4 - bugs
+//змінна х не має нічого в собі
+//крапка з комою при реалізації функції не повинна стояти після () таких дужок
