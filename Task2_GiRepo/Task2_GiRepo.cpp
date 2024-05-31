20e5be7f9429b7cb219ec23bb545b85d9c2b1b2c
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-05-31 18:39:01 +0200

    HW_9 - references

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,47 +1,91 @@
 #include <iostream>
 using namespace std;
 
+class Cat
+{
+public:
+	Cat(int age) { itsAge = age; }
+	~Cat() {}
+	int GetAge() const { return itsAge; }
+
+private:
+	int itsAge;
+};
+
+Cat* MakeCat(int age);
+
 int main() {
-	//task 1 
-	//int *pOne; - оголошення вказівника на тип інт
-	//int vTwo; - оголошення змінної типу інт
-	//int *pThree = &vTwo; - оголошення вказівника типу інт, який вказує на адресу змінної vTwo
+	//task 1
+	int a = 5;
+	int& b = a;
+	int* c = &a;
+
+	cout << "A: " << a << endl;
+	cout << "&B: " << b << endl;
+	cout << "*C: " << *c << endl;
+
+	cout << endl << "Change pointer" << endl;
+
+	b = 7;
+
+	cout << "A: " << a << endl;
+	cout << "&B: " << b << endl;
+	cout << "*C: " << *c << endl;
+
+	cout << endl << "Change ref" << endl;
+
+	b = 10;
+
+	cout << "A: " << a << endl;
+	cout << "&B: " << b << endl;
+	cout << "*C: " << *c << endl;
 
 	//task 2
-	//unsigned short yourAge;
-	//unsigned short *pYourAge = nullptr;
-	//pYourAge = &yourAge;
+
+	/*int varOne = 10;
+	const int* pointer = new const int;
+	pointer = varOne;
+
+	cout << "First iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
+
+	varOne = 6;
+
+	cout << "Second iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
+
+	pointer = 7;
+
+	cout << "Third iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
+
+	int varTwo = 20;
+	pointer = varTwo;
+	cout << "Fourth iteration: \tVarOne: " << varOne << "\tPointer a: " << a << endl;*/
 
 	//task 3
-	int* a = nullptr;
-	int b = 0;
-	a = &b;
-
-	cout << "B: " << b << endl;
-	cout << "*A: " << *a << endl;
-
-	b = 50;
-	cout << "B = 50; B: " << b << endl;
-	cout << "*A: " << *a << endl;
+	//так як вказіник константний ві повинен бути відразу ініціалізований і не повинен змінюватись в процесі програми.
 
 	//task 4
-	int var1;
-	int* pVar1 = &var1;
-
-	*pVar1 = 100;
-	cout << "Var 1 : " << var1 << endl;
-
+	int* strayPointer = new int;
+	delete strayPointer;
 	//task 5
-	//int* pInt;
-	//*pInt = 9;
-	//cout << " The value at pInt: " << *pInt;
-	//вказівник потрібно ініціалізовувати як нуль поінтер або посилання на якусь змінну
+	strayPointer = nullptr;
 
 	//task 6
-	int SomeVariable = 5;
-	cout << "SomeVariable: " << SomeVariable << "\n";
-	int* pVar = &SomeVariable;
-	*pVar = 9; //-правильно було б написати * pVar = 9;
-	cout << "SomeVariable: " << *pVar << "\n";
+	int* memoryLeakP = new int;
+	*memoryLeakP = 78;
+	//task 7
+	delete strayPointer;
 
+	int age = 7;
+	Cat *Boots = MakeCat(age);
+	cout << "Boots is: " << Boots->GetAge() << " years old" << endl;
+
+	delete Boots;
+	Boots = nullptr;
+	return 0;
+}
+
+Cat* MakeCat(int age)//dangerous method - can couse memory leak
+{
+	Cat* pCat = new Cat(age);
+	return pCat;
 }
