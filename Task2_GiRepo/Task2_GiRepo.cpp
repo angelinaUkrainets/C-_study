760a20a3ce171e5fb95c93cfe34e9a4a43f37a3f
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-06-11 18:30:04 +0200

    HW 13 - arrays

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 using namespace std;
 
-//task 1 - its impossible to make virtual constructors in cpp
-class Rectangle {
-
-protected: 
-	Rectangle() {
-		cout << "Constructor works" << endl;
-	}
-	~Rectangle()
-	{
-		cout << "Destructor works" << endl;
-	}
-};
-
-class Square :public Rectangle {
-public:
-	Square() {
-		cout << "Constructor square" << endl;
-	}
-	~Square()
-	{
-		cout << "Destructor square" << endl;
-	}
-};
-
-//task 3 
-class Shape
-{
-public:
-	Shape();
-	virtual ~Shape();
-	Shape(const Shape&); //віртуальні конструктори не можна використовувати в с++
-};
-
 
 int main() {
+	//task 1
+	char field[3][3];
+	for(int i = 0; i<3; i++)
+		for (int j = 0; j < 3; j++)
+		{
+			if (i % 2 == 0 && j % 2 == 0)
+				field[i][j] = 'X';
+			else
+				field[i][j] = 'O';
+		}
 
-	//task 2 
-	//void SomeFunction(Shape);
-	//Shape* pRect = new Rectangle; - для такого вказування, клас прямокутник має бути успадкований від класу фігура
-	//SomeFunction(*pRect); - даний виклик функції є некоректним
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << " " << field[i][j] << " |";
+		}
+		cout << endl << "____________" << endl;
+	}
 
+	//task 2
+	cout << "\nTask 2 - inicialising '0'\n" << endl;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			field[i][j] = 'O';
 
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << " " << field[i][j] << " |";
+		}
+		cout << endl << "____________" << endl;
+	}
+
+	//task 3 - виходить за межі масиву в циклі, і має бути до 5, а j до 4
+	unsigned short SomeArray[5][4];
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 5; j++)
+			SomeArray[i][j] = i + j;
+
+	//task 4 - також виходить за межі, треба змінити знак на < у двох циклах
+	unsigned short SomeArray[5][4];
+	for (int i = 0; i <= 5; i++)
+		for (int j = 0; j <= 4; j++)
+			SomeArray[i][j] = 0;
 }
