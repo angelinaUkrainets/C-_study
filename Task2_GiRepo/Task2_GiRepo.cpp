dae2c4499ad6ce6af02ff24e58eb059e8e4e4ac3
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-06-11 18:04:34 +0200

    HW 12 - inheritance

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,91 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class Cat
-{
-public:
-	Cat(int age) { itsAge = age; }
-	~Cat() {}
-	int GetAge() const { return itsAge; }
+//task 1 - its impossible to make virtual constructors in cpp
+class Rectangle {
 
-private:
-	int itsAge;
+protected: 
+	Rectangle() {
+		cout << "Constructor works" << endl;
+	}
+	~Rectangle()
+	{
+		cout << "Destructor works" << endl;
+	}
 };
 
-Cat* MakeCat(int age);
+class Square :public Rectangle {
+public:
+	Square() {
+		cout << "Constructor square" << endl;
+	}
+	~Square()
+	{
+		cout << "Destructor square" << endl;
+	}
+};
+
+//task 3 
+class Shape
+{
+public:
+	Shape();
+	virtual ~Shape();
+	Shape(const Shape&); //віртуальні конструктори не можна використовувати в с++
+};
+
 
 int main() {
-	//task 1
-	int a = 5;
-	int& b = a;
-	int* c = &a;
 
-	cout << "A: " << a << endl;
-	cout << "&B: " << b << endl;
-	cout << "*C: " << *c << endl;
+	//task 2 
+	//void SomeFunction(Shape);
+	//Shape* pRect = new Rectangle; - для такого вказування, клас прямокутник має бути успадкований від класу фігура
+	//SomeFunction(*pRect); - даний виклик функції є некоректним
 
-	cout << endl << "Change pointer" << endl;
 
-	b = 7;
-
-	cout << "A: " << a << endl;
-	cout << "&B: " << b << endl;
-	cout << "*C: " << *c << endl;
-
-	cout << endl << "Change ref" << endl;
-
-	b = 10;
-
-	cout << "A: " << a << endl;
-	cout << "&B: " << b << endl;
-	cout << "*C: " << *c << endl;
-
-	//task 2
-
-	/*int varOne = 10;
-	const int* pointer = new const int;
-	pointer = varOne;
-
-	cout << "First iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
-
-	varOne = 6;
-
-	cout << "Second iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
-
-	pointer = 7;
-
-	cout << "Third iteration: \tVarOne: " << varOne << "\tPointer a: " << pointer << endl;
-
-	int varTwo = 20;
-	pointer = varTwo;
-	cout << "Fourth iteration: \tVarOne: " << varOne << "\tPointer a: " << a << endl;*/
-
-	//task 3
-	//так як вказіник константний ві повинен бути відразу ініціалізований і не повинен змінюватись в процесі програми.
-
-	//task 4
-	int* strayPointer = new int;
-	delete strayPointer;
-	//task 5
-	strayPointer = nullptr;
-
-	//task 6
-	int* memoryLeakP = new int;
-	*memoryLeakP = 78;
-	//task 7
-	delete strayPointer;
-
-	int age = 7;
-	Cat *Boots = MakeCat(age);
-	cout << "Boots is: " << Boots->GetAge() << " years old" << endl;
-
-	delete Boots;
-	Boots = nullptr;
-	return 0;
-}
-
-Cat* MakeCat(int age)//dangerous method - can couse memory leak
-{
-	Cat* pCat = new Cat(age);
-	return pCat;
 }
