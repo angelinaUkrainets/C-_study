44aab2865732a16154be9d701d6c69fda3eacb7f
AngelinaUkrainets <angelyusha.u@gmail.com>
2024-06-18 19:35:22 +0200

    HW 14 - strings and lists

--- Task2_GiRepo/Task2_GiRepo.cpp
+++ Task2_GiRepo/Task2_GiRepo.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+private:
+	int value;
+	Node* next;
+	Node* prev;
+
+public:
+	Node(int value) {
+		this->value = value;
+		this->next = nullptr;
+		this->prev = nullptr;
+	}
+};
+
 
 int main() {
-	//task 1
-	char field[3][3];
-	for(int i = 0; i<3; i++)
-		for (int j = 0; j < 3; j++)
-		{
-			if (i % 2 == 0 && j % 2 == 0)
-				field[i][j] = 'X';
-			else
-				field[i][j] = 'O';
-		}
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << " " << field[i][j] << " |";
-		}
-		cout << endl << "____________" << endl;
-	}
-
-	//task 2
-	cout << "\nTask 2 - inicialising '0'\n" << endl;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			field[i][j] = 'O';
-
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << " " << field[i][j] << " |";
-		}
-		cout << endl << "____________" << endl;
-	}
-
-	//task 3 - виходить за межі масиву в циклі, і має бути до 5, а j до 4
-	unsigned short SomeArray[5][4];
-	for (int i = 0; i < 4; i++)
-		for (int j = 0; j < 5; j++)
-			SomeArray[i][j] = i + j;
-
-	//task 4 - також виходить за межі, треба змінити знак на < у двох циклах
-	unsigned short SomeArray[5][4];
-	for (int i = 0; i <= 5; i++)
-		for (int j = 0; j <= 4; j++)
-			SomeArray[i][j] = 0;
 }
